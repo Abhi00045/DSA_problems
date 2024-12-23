051c8c5126471351c7631bcb9e0ffce9b48e8879
abhi <rajidiabhinavreddy@gmail.com>
2024-12-24 01:57:09 +0530

    2 am done

---
+++ DSA/src/day1.java
@@ -0,0 +1,64 @@
+
+public class day1 {
+    public static void main(String[] args) {
+
+//basic -----------------------------------------------------------------------
+
+        System.out.println("Hello, World!");
+
+        // String abhi = "boy";
+        // if(abhi.equals("boy")){
+        //     System.err.println("abhi is a boy");
+        // }else{
+        //     System.err.println("abhi is  a girl and not a boy");
+        // }
+        // arrays(args);
+
+
+//inputs for java --------------------------------------------------------------
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the size of array: ");
+        // int n = sc.nextInt();
+        // int num[] = new int[n];
+        // System.out.println("Enter the elements of array: ");
+        // for(int i=0;i<n;i++){
+        //     num[i] = sc.nextInt();
+        //     System.err.println(num[i]);
+        // }
+
+//arrays -----------------------------------------------------------------------
+//time_complexity_----------------------------------------
+ // arrays ------------sorting-----------------
+    //     int arr[] = {3,4,1,7,3,7,2,8,5};
+    //     for(int i=0;i<arr.length;i++){
+    //         // System.out.println(arr[i]);
+    //         int start = 0;
+    //         int end = start+1;
+    //         if(arr[start]<arr[end]){
+    //             swap(arr,start,end);
+    //             start++;
+    //         }else{
+    //             end++;
+    //         }
+    //     }
+    //     for(int i=0;i<arr.length;i++){
+    //         System.out.println(arr[i]);
+    //     }
+    // }
+    // public static void swap(int arr[], int i,int j){
+    //     int k = arr[i];
+    //     arr[i] = arr[j];
+    //     arr[j] = k;
+
+ //arrays -------------- merge,quick---------------------
+ //arrays --------------searching------------------------
+
+
+
+    // }
+    //function recalling --------------------------------------------------------------
+    // public static void arrays(String[] args) {
+        // System.out.println("Hello, World!");
+    // }
+    }
+}
--- reactjs/src/Home.jsx
+++
@@ -1,9 +0,0 @@
-const Home = ()=>{
-
-    return(
-        <>
-        <div className="home-img"></div>
-        </>
-    )
-}
-export default Home;
